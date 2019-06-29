38a790b817534a6c8ec531d22493cb7379edd366
Barış HIRECE <brshrce@gmail.com>
2019-06-29 17:20:43 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,3 +123,4 @@ TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brigh
 TW_MAX_BRIGHTNESS := 255
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_DEVICE_VERSION := 0_barishirece
